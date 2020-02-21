140b5a8a789312ee84979fd62d6977eb0bdab3f0
unknown <mhughes.ipod@gmail.com>
2020-02-20 21:30:58 -0800

    init

--- frontend/frontend/frontend/src/components/Article.js
+++ frontend/frontend/frontend/src/components/Article.js
@@ -39,12 +39,59 @@ class Article extends Component {
 		// 		}
 		// 	];
 		// }
+		const today = Date();
+		let events = [];
+		function MonthlyEvent(name, start) {
+			(this.name = name), (this.start_date = start), (this.dates = []);
+
+			let pay_date = Date();
+			pay_date.setDate(start);
+			this.dates.push(pay_date);
+			for (let i = 0; i < 10; i++) {
+				pay_date.setMonth(i % 11);
+				this.dates.push(pay_date);
+				console.log(pay_date);
+			}
+		}
+		const weekly_event = {
+			constructor(start) {
+				let pay_date = Date();
+				pay_date.setDate(start);
+				this.dates.push(pay_date);
+				for (let i = 0; i < 10; i++) {
+					pay_date.setDate(pay_date.getDate + 7);
+					this.dates.push(pay_date);
+					console.log(pay_date);
+				}
+			},
+			name: 'monthly event',
+			start_date: 0,
+			dates: []
+		};
+		const create_event = (name, type, start) => {
+			if ((type = 'monthly')) {
+				const event = new MonthlyEvent(name, start);
+				event.name = name;
+				return event;
+			} else if ((type = 'weekly')) {
+				const event = weekly_event(start);
+				event.name = name;
+				return event;
+			} else {
+				alert('Not a valid event type.');
+			}
+		};
+
+		const gym = create_event('Club Sport', 'monthly', 5);
 
 		return (
 			<div>
-				<h3>{this.state.data[0].title}</h3>
+				{/* <h3>{this.state.data[0].title}</h3>
 				<p>{this.state.data[0].created_at}</p>
-				<p>{this.state.data[0].message}</p>
+				<p>{this.state.data[0].message}</p> */}
+				<h3>{gym.name}</h3>
+				<h3>{gym.start_date}</h3>
+				<h3>{gym.dates[0]}</h3>
 			</div>
 		);
 	}
